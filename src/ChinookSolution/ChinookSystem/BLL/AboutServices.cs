7c1a27fcbb8dede61b6a7e66bb380e8628bb8cb1
jaredj13 <jaredj9914@hotmail.com>
2022-11-16 09:45:06 -0700

    ViewModels

---
+++ src/ChinookSolution/ChinookSystem/BLL/AboutServices.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region AdditionalNamespaces
+using ChinookSystem.DAL;
+using ChinookSystem.Entities;
+using ChinookSystem.ViewModels;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    public class AboutServices
+    {
+        // This class needs to be accessed by an "outside user" (WebApp)
+        //  Therefore the class needs to be public
+
+        #region Constructor and Context Dependency
+        private readonly Chinook2018Context _context;
+        internal AboutServices(Chinook2018Context context)
+        {
+            _context = context;
+        }
+
+        #endregion
+
+        #region Services
+        // Services are methods
+
+        // Query to obtain the DbVersion data
+        public DbVersionInfo GetDbVersion()
+        {
+            DbVersionInfo info = _context.DbVersions
+                            .Select(x => new DbVersionInfo
+                            {
+                                Major = x.Major,
+                                Minor = x.Minor,
+                                Build = x.Build,
+                                ReleaseDate = x.ReleaseDate
+                            })
+                            .SingleOrDefault();
+            return info;
+        }
+        #endregion
+    }
+}
---
+++ src/ChinookSolution/ChinookSystem/ViewModels/DbVersionInfo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.ViewModels
+{
+    public class DbVersionInfo
+    {
+        // The view that is used by the "outside world"
+        //  The access must match where the class is used (typically public)
+        // Purpose: Used to simply carry data
+        //          Creates data fields as auto-implemented properties
+        //          Consists of the "raw" data on the query
+        //          No validation done here
+
+        public int Major { get; set; }
+        public int Minor { get; set; }
+        public int Build { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
